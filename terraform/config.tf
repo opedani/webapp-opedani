e81cbef34d2f7748b39cb277ee88ada4d50b27e0
KageKowalski <KageKowalski@gmail.com>
2022-06-03 15:15:49 -0700

    Created basic Terraform code for managing AWS account.

---
+++ terraform/config.tf
@@ -0,0 +1,20 @@
+# Terraform and provider configurations
+
+
+# Terraform configuration
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.17.0"
+    }
+  }
+}
+
+
+# AWS configuration
+provider "aws" {
+  region     = "us-east-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+# Variable declarations
+
+
+variable "aws_access_key" {}  # Provider access key
+variable "aws_secret_key" {}  # Provider secret key
