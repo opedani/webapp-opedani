a33d9b6b5d205e032193135c6443bd67985c5c1b
KageKowalski <KageKowalski@gmail.com>
2022-06-05 15:47:54 -0700

    Began working on SG and SG rules

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,23 +1,48 @@
 # Security Rules and Groups
 
 
-#resource "aws_security_group" "web_server_http" {
-#  source = "terraform-aws-modules/security-group/aws//modules/http-80"
-#
-#  name        = "web_server_http"
-#  description = "Security group for web-server with HTTP ports open within VPC"
-#  vpc_id      = aws_eip.web_server_eip.vpc
-#
-#  ingress_cidr_blocks = ["0.0.0.0/0"]
-#}
-#
-#
-#resource "aws_security_group" "web-server_https" {
-#  source = "terraform-aws-modules/security-group/aws//modules/https-443"
-#
-#  name   = "web_server_https"
-#  description = "Security group for web-server with HTTP ports open within VPC"
-#  vpc_id      = aws_eip.web_server_eip.vpc
-#
-#  ingress_cidr_blocks = ["0.0.0.0/0"]
-#}
+resource "aws_security_group" "web_server_sg" {
+  name        = "web_server_sg"
+  description = "Security Group for web server"
+  vpc_id      = aws_eip.web_server_eip.id
+}
+
+
+resource "aws_security_group_rule" "http_in" {
+  description       = "Allows http traffic in"
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web_server_sg.id
+}
+
+
+resource "aws_security_group_rule" "http_out" {
+  description       = "Allows http traffic out"
+  type              = "egress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web_server_sg.id
+}
+
+
+resource "aws_security_group_rule" "https_in" {
+  description       = "Allows https traffic in"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web_server_sg.id
+}
+
+
+resource "aws_security_group_rule" "https_out" {
+  description       = "Allows https traffic out"
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  security_group_id = aws_security_group.web_server_sg.id
+}
