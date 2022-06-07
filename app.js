768cb1f85ff3b6d021413627092e88dae59be881
Klayton Kowalski <klaytonkowalski@gmail.com>
2022-06-07 01:04:48 -0500

    Added autocomplete box on home page. Successfully filtering anime names from animenewsnetwork API.

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 
 const express = require('express')
 const path = require('path')
-
-const indexRouter = require('./routes/index.js')
+const https = require('https')
+const xml2js = require('xml2js')
 
 ////////////////////////////////////////////////////////////////////////////////
 // PROPERTIES
@@ -13,9 +13,47 @@ const indexRouter = require('./routes/index.js')
 
 const app = express()
 const port = 3000
+const xmlParser = new xml2js.Parser({ attrkey: 'ATTR' })
+
+let animeNames = []
 
 ////////////////////////////////////////////////////////////////////////////////
-// EXECUTE
+// FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////
+
+function updateAnimeNames()
+{
+    https.get('https://www.animenewsnetwork.com/encyclopedia/reports.xml?id=155&type=anime&nlist=all', (res) =>
+    {
+        let data = ''
+        res.on('data', (stream) =>
+        {
+            data += stream
+        })
+        res.on('end', () =>
+        {
+            xmlParser.parseString(data, (error, result) =>
+            {
+                if (error)
+                {
+                    console.log('FAILURE: updateAnimeNames()', error)
+                }
+                else
+                {
+                    animeNames = []
+                    const animes = result.report.item
+                    for (const anime of animes)
+                    {
+                        animeNames.push(anime.name[0])
+                    }
+                }
+            })
+        })
+    })
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// SETUP
 ////////////////////////////////////////////////////////////////////////////////
 
 app.set('views', path.join(__dirname, 'views'))
@@ -25,6 +63,28 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-
 app.listen(port, () => console.log(`Launched OpEdAni at http://localhost:3000.`))
+
+updateAnimeNames()
+setInterval(updateAnimeNames, 86400000)
+
+////////////////////////////////////////////////////////////////////////////////
+// RESPONSES
+////////////////////////////////////////////////////////////////////////////////
+
+function getIndexPage(req, res)
+{
+    res.render('index')
+}
+
+function getAnimeNames(req, res)
+{
+    res.json(animeNames)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// ROUTES
+////////////////////////////////////////////////////////////////////////////////
+
+app.get('/', getIndexPage)
+app.get('/api/get-anime-names', getAnimeNames)
