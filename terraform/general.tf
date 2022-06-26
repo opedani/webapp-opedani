b5037379c65a05cb1910328cd6a5b9a38ea7dc4c
KageKowalski <KageKowalski@gmail.com>
2022-06-25 18:50:55 -0700

    Revert "Attempted to make Ansible inventory dynamic"
    This reverts commit b1f67de44479ab3cbbf047368cd5d5c78825102e.

--- terraform/general.tf
+++ terraform/general.tf
@@ -24,14 +24,3 @@ resource "aws_key_pair" "ansible_ssh_key" {
   key_name   = "ansible.pub"
   public_key = file("data/ansible.pub")
 }
-
-# Generate inventory file for Ansible
-resource "local_file" "ansible_inventory" {
-  filename = "../ansible/.inventory"
-  content = <<EOF
-[apache]
-${aws_eip.apache_eip.public_ip}
-[node]
-${aws_eip.node1_eip.public_ip}
-EOF
-}
