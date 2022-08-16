c83f2cb350e2dbd172867092eaf2067d3087784d
Klayton Kowalski <klaytonkowalski@gmail.com>
2022-08-15 20:41:18 -0400

    Progress on search results container.

--- public/scripts/layout.js
+++ public/scripts/layout.js
@@ -1,16 +1,17 @@
 const searchForm = $('#layout-search-form')
 const searchQuery = $('#layout-search-query')
+const searchResultContainer = $('#layout-result-container')
 
 let searchFormTimeout
 
-searchForm.on('input', () =>
+function searchForm_onInput()
 {
+    if (searchFormTimeout)
+    {
+        clearTimeout(searchFormTimeout)
+    }
     if (searchQuery.val().length >= 3)
     {
-        if (searchFormTimeout)
-        {
-            clearTimeout(searchFormTimeout)
-        }
         searchFormTimeout = setTimeout(() =>
         {
             const request =
@@ -22,20 +23,50 @@ searchForm.on('input', () =>
                 },
                 success: response =>
                 {
-                    
+                    searchResultContainer.empty()
+                    searchResultContainer.toggleClass('hidden', response.length == 0)
+                    for (const anime of response)
+                    {
+                        searchResultContainer.append(`
+                            <a class="flex-row block-link relative" href="/anime/${anime.id}">
+                                <img class="layout-result-thumbnail thumbnail" src="${anime.thumbnail}" alt="<Thumbnail>">
+                                <div class="layout-result-body">
+                                    <div class="padding">${anime.titles[0]}</div>
+                                    <div class="layout-result-statistics">
+                                        <div class="icon-text">
+                                            <i class="fa-solid fa-star"></i>
+                                            <p>0.00</p>
+                                        </div>
+                                        <div class="icon-text">
+                                            <i class="fa-solid fa-ranking-star"></i>
+                                            <p>#1000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </a>
+                        `)
+                    }
                 }
             }
             $.ajax(request)
         },
-        1000)
+        500)
     }
-})
+    else
+    {
+        searchResultContainer.empty()
+        searchResultContainer.toggleClass('hidden', true)
+    }
+}
 
-searchForm.on('submit', event =>
+function searchForm_onSubmit(event)
 {
     event.preventDefault()
     if (searchQuery.val().length >= 3)
     {
         location.href = `${location.origin}/search?query=${searchQuery.val()}`
     }
-})
+}
+
+searchForm.on('input', searchForm_onInput)
+searchForm.on('submit', searchForm_onSubmit)
