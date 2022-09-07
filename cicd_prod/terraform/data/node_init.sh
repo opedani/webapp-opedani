9c7a2baed1e5b75f7d7ac1e3129d6c7a1ae98af0
KageKowalski <KageKowalski@gmail.com>
2022-09-07 15:22:29 -0700

    Syntax error, extra colon

--- cicd_prod/terraform/data/node_init.sh
+++ cicd_prod/terraform/data/node_init.sh
@@ -27,7 +27,7 @@ module.exports = {
     name               : 'webapp-opedani',
     script             : 'webapp-opedani/app.js',
     watch              : ['webapp-opedani'],
-    watch_delay:       : 60,
+    watch_delay        : 60,
     instances          : 'max',
     max_memory_restart : '256M'
   }]
