c40d3baf47062d0ab43205c9b2eb76677f1e1170
KageKowalski <KageKowalski@gmail.com>
2022-09-11 17:16:46 -0700

    terraform fmt

--- cicd_dev/terraform/vpc.tf
+++ cicd_dev/terraform/vpc.tf
@@ -3,14 +3,14 @@
 
 # VPC
 resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
 
 
 # Public Subnet, Internet Gateway, Route Table, and Association
 resource "aws_subnet" "public_subnet" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.0.0/24"
 }
 resource "aws_internet_gateway" "internet_gateway" {
@@ -25,32 +25,32 @@ resource "aws_route_table" "public_subnet_route_table" {
   }
 }
 resource "aws_route_table_association" "public_subnet-public_subnet_route_table" {
-  subnet_id = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_subnet_route_table.id
 }
 
 
 # Private Subnet, NAT Gateway (with EIP), Route Table, and Association
 resource "aws_subnet" "private_subnet" {
-  vpc_id = aws_vpc.vpc.id
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
 }
 resource "aws_eip" "nat_gateway_eip" {
   vpc = true
 }
-resource "aws_nat_gateway" "nat_gateway"{
+resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway_eip.id
-  subnet_id = aws_subnet.public_subnet
+  subnet_id     = aws_subnet.public_subnet
 }
 resource "aws_route_table" "private_subnet_route_table" {
   vpc_id = aws_vpc.vpc.id
-  
+
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat_gateway.id
   }
 }
 resource "aws_route_table_association" "private_subnet-private_subnet_route_table" {
-  subnet_id = aws_subnet.private_subnet.id
+  subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_subnet_route_table.id
 }
