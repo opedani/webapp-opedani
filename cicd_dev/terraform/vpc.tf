e015ca34cd370ac9248135a2cf8f40ce50b12ebe
KageKowalski <KageKowalski@gmail.com>
2022-09-11 17:15:24 -0700

    Added basic public and private VPC to Dev infrastructure

---
+++ cicd_dev/terraform/vpc.tf
@@ -0,0 +1,56 @@
+# Terraform code specific to the VPC
+
+
+# VPC
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+}
+
+
+# Public Subnet, Internet Gateway, Route Table, and Association
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/24"
+}
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.vpc.id
+}
+resource "aws_route_table" "public_subnet_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+resource "aws_route_table_association" "public_subnet-public_subnet_route_table" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_subnet_route_table.id
+}
+
+
+# Private Subnet, NAT Gateway (with EIP), Route Table, and Association
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+resource "aws_eip" "nat_gateway_eip" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat_gateway"{
+  allocation_id = aws_eip.nat_gateway_eip.id
+  subnet_id = aws_subnet.public_subnet
+}
+resource "aws_route_table" "private_subnet_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
+resource "aws_route_table_association" "private_subnet-private_subnet_route_table" {
+  subnet_id = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_subnet_route_table.id
+}
