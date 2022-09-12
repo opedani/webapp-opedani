1a22890fb01a81129ad323c50db771f08a278dbf
KageKowalski <KageKowalski@gmail.com>
2022-09-11 23:59:51 -0700

    terraform fmt

--- cicd_dev/terraform/vpc.tf
+++ cicd_dev/terraform/vpc.tf
@@ -20,9 +20,9 @@ resource "aws_route_table" "public_subnet_route_table" {
   vpc_id = aws_vpc.vpc.id
 }
 resource "aws_route" "public_subnet_ig_route" {
-  route_table_id = aws_route_table.public_subnet_route_table.id
+  route_table_id         = aws_route_table.public_subnet_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.internet_gateway.id
+  gateway_id             = aws_internet_gateway.internet_gateway.id
 }
 resource "aws_route_table_association" "public_subnet-public_subnet_route_table" {
   subnet_id      = aws_subnet.public_subnet.id
@@ -46,9 +46,9 @@ resource "aws_route_table" "private_subnet_route_table" {
   vpc_id = aws_vpc.vpc.id
 }
 resource "aws_route" "private_subnet_nat_route" {
-  route_table_id = aws_route_table.private_subnet_route_table.id
+  route_table_id         = aws_route_table.private_subnet_route_table.id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
 }
 resource "aws_route_table_association" "private_subnet-private_subnet_route_table" {
   subnet_id      = aws_subnet.private_subnet.id
