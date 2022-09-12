298523ec6cb69cda670a182fa52833dde1d0bfeb
KageKowalski <KageKowalski@gmail.com>
2022-09-12 09:25:22 -0700

    SG and SG Rules associated with node1_ec2 are now named node* rather than node1* because the same SG will be applied to future Node servers

--- cicd_dev/terraform/node_servers.tf
+++ cicd_dev/terraform/node_servers.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "node1_ec2" {
   ami                         = data.aws_ami.amazon_linux_2_ami.id
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.ansible_ssh_key.key_name
-  vpc_security_group_ids      = [aws_security_group.node1_sg.id]
+  vpc_security_group_ids      = [aws_security_group.node_sg.id]
   subnet_id                   = aws_subnet.public_subnet.id
   associate_public_ip_address = true
 }
@@ -20,80 +20,80 @@ resource "aws_eip" "node1_eip" {
 
 
 # Security Group and Rules for node1 server
-resource "aws_security_group" "node1_sg" {
+resource "aws_security_group" "node_sg" {
   vpc_id      = aws_vpc.vpc.id
   name        = "node1_sg"
   description = "Security Group for node1 server"
 }
-resource "aws_security_group_rule" "node1_sg_http_in" {
+resource "aws_security_group_rule" "node_sg_http_in" {
   description       = "Allows http traffic in"
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_http_out" {
+resource "aws_security_group_rule" "node_sg_http_out" {
   description       = "Allows http traffic out"
   type              = "egress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_https_in" {
+resource "aws_security_group_rule" "node_sg_https_in" {
   description       = "Allows https traffic in"
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_https_out" {
+resource "aws_security_group_rule" "node_sg_https_out" {
   description       = "Allows https traffic out"
   type              = "egress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_ssh_in" {
+resource "aws_security_group_rule" "node_sg_ssh_in" {
   description       = "Allows ssh traffic in"
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_ssh_out" {
+resource "aws_security_group_rule" "node_sg_ssh_out" {
   description       = "Allows ssh traffic out"
   type              = "egress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_3000_in" {
+resource "aws_security_group_rule" "node_sg_3000_in" {
   description       = "Allows traffic in on port 3000"
   type              = "ingress"
   from_port         = 3000
   to_port           = 3000
   protocol          = "tcp"
   cidr_blocks       = ["${aws_eip.apache_eip.public_ip}/32"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
-resource "aws_security_group_rule" "node1_sg_3000_out" {
+resource "aws_security_group_rule" "node_sg_3000_out" {
   description       = "Allows traffic out on port 3000"
   type              = "egress"
   from_port         = 3000
   to_port           = 3000
   protocol          = "tcp"
   cidr_blocks       = ["${aws_eip.apache_eip.public_ip}/32"]
-  security_group_id = aws_security_group.node1_sg.id
+  security_group_id = aws_security_group.node_sg.id
 }
