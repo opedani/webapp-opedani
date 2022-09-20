68a9a413e60a52864848ba1532c949259f49fbd5
KageKowalski <KageKowalski@gmail.com>
2022-09-19 21:57:11 -0700

    Updated ecosystem.config.js for main.js

--- cicd_dev/ansible/data/ecosystem.config.js
+++ cicd_dev/ansible/data/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name               : 'webapp-opedani',
-    script             : 'webapp-opedani/app.js',
+    script             : 'webapp-opedani/main.js',
     watch              : ['webapp-opedani'],
     watch_delay        : 60,
     instances          : 'max',
