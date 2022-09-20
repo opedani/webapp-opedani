992d790689a84a6e23da97f15a9445947401b998
KageKowalski <KageKowalski@gmail.com>
2022-09-19 19:49:19 -0700

    terraform fmt

--- cicd_dev/terraform/variables.tf
+++ cicd_dev/terraform/variables.tf
@@ -1,6 +1,6 @@
 # Variable declarations
 
 
-variable "aws_access_key" {} # Provider access key
-variable "aws_secret_key" {} # Provider secret key
+variable "aws_access_key" {}    # Provider access key
+variable "aws_secret_key" {}    # Provider secret key
 variable "aurora_cluster_pw" {} # Master password for Aurora cluster
