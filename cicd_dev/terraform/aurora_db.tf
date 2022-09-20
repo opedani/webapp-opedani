c63b24c799e5728b17ecda7c6f20d7936bd06795
KageKowalski <KageKowalski@gmail.com>
2022-09-19 21:55:06 -0700

    Added more params to Aurora cluster

--- cicd_dev/terraform/aurora_db.tf
+++ cicd_dev/terraform/aurora_db.tf
@@ -3,13 +3,22 @@
 
 # Aurora RDS Cluster
 resource "aws_rds_cluster" "aurora_cluster" {
+  # Rarely changed critical input
   cluster_identifier = "aurora-cluster"
   engine             = "aurora-postgresql"
   engine_mode        = "provisioned"
   engine_version     = "13.6"
   database_name      = "aurora-db"
   master_username    = "postgres"
-  master_password    = "PLACE_PASSWORD_HERE"
+  master_password    = var.aurora_cluster_pw
+
+  # Rarely changed non-critical input
+  backup_retention_period = 7
+
+  # Changeable input
+  allow_major_version_upgrade = false
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.aurora_cluster_param_grp
+  db_instance_parameter_group_name = aws_db_parameter_group.aurora_db_param_grp
 
   serverlessv2_scaling_configuration {
     max_capacity = 1.0
@@ -19,9 +28,25 @@ resource "aws_rds_cluster" "aurora_cluster" {
 
 
 # Aurora RDS Cluster Instance
-resource "aws_rds_cluster_instance" "example" {
+resource "aws_rds_cluster_instance" "aurora_cluster_instance" {
   cluster_identifier = aws_rds_cluster.aurora_cluster.id
   instance_class     = "db.serverless"
   engine             = aws_rds_cluster.aurora_cluster.engine
   engine_version     = aws_rds_cluster.aurora_cluster.engine_version
+}
+
+
+# Aurora Cluster Parameter Group
+resource "aws_rds_cluster_parameter_group" "aurora_cluster_param_grp" {
+  name        = "aurora-cluster-parameter-group"
+  family      = "aurora-postgresql13"
+  description = "aurora-cluster-parameter-group"
+}
+
+
+# Aurora DB Parameter Group
+resource "aws_db_parameter_group" "aurora_db_param_grp" {
+  name        = "aurora-db-parameter-group"
+  family      = "aurora-postgresql13"
+  description = "aurora-db-parameter-group"
 }
